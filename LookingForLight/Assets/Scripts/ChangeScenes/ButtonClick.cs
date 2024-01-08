2ff14591160584e6f8a9a8f5e57c50dfdabe7c92
pribishlove <m.makushkin2004@gmail.com>
2024-01-08 15:05:18 +0500

    Fixed3

--- LookingForLight/Assets/Scripts/ChangeScenes/ButtonClick.cs
+++ LookingForLight/Assets/Scripts/ChangeScenes/ButtonClick.cs
@@ -4,8 +4,17 @@ using UnityEngine.SceneManagement;
 public class ButtonClick : MonoBehaviour
 {
     // Метод, который будет вызываться при нажатии кнопки
+
+    private GameManager2 gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager2>();
+    }
+
     public void OnButtonClick()
     {
+        gameManager.SaveScene();
         // Здесь вы можете указать имя сцены, на которую вы хотите перейти
         string sceneToLoad = "Main-menu";
 
